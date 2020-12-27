5741dca9d10c70278883fb6d60c7bb4184bc0946
pauloabr18 <cnnick.ex@gmail.com>
2020-12-26 20:53:58 -0600

    lol

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,7 +1,14 @@
 def main():
-    
-    pass
-
+    txt = open("../codes.txt")
+    l = {}
+    for list in txt:
+        i = int(list)
+        if i in l:
+            y = 2020 - i
+            l[y] = i
+        else:
+            print("{0} + {1} = 2020\n{2} * {3} = {4}".format(i, l[i], i, l[i], i*l[i]))
+        return 0
+        
 if __name__ == "__main__":
     main()
-    pass
